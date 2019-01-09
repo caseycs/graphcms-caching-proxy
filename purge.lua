d07040ec201964843e805ba899e669732a32d8fe
Ilia Kondrashov <caseycs@gmail.com>
2019-01-09 14:14:27 +0100

    add removed cache items in response header

--- purge.lua
+++ purge.lua
@@ -8,11 +8,16 @@ function exec(cmd)
         local result = handle:read("*all")
         handle:close()
         ngx.log(ngx.INFO, result)
+        return result
 end
 
 if ngx ~= nil then
-        local cmd = "/usr/bin/find " .. ngx.var.lua_purge_path .. " -type f -delete -print0 2>&1"
-        exec(cmd)
+        local cmd = "/usr/bin/find " .. ngx.var.lua_purge_path .. " -type f -delete -print 2>&1"
+        local result = exec(cmd)
+        local lines  = select(2, string.gsub(result, '\n', ""))
+
+        ngx.header["Content-type"] = "text/plain; charset=utf-8"
+        ngx.header["X-Purged-Count"] = lines
 
         ngx.say("OK")
         ngx.exit(ngx.OK)
